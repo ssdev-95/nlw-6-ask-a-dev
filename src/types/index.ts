3afc8e2ca058236cfe885efcaf9f97544ef75ee5
xSallus <souza95salomao@gmail.com>
2021-06-23 15:30:32 -0400

    [UPDATE] Types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,9 @@ import { ReactNode } from 'react';
 export interface ProviderProps {
     children: ReactNode;
 }
+
+export interface ButtonProps {
+    children: ReactNode;
+    callback?: ()=>void|undefined;
+    bgcolor?: string;
+}
