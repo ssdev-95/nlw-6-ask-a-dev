6d8ef73409aa09881ec040efd620ff2f6496a8c7
xSallus <souza95salomao@gmail.com>
2021-06-23 15:29:55 -0400

    [UPDATE] Firebase config

--- src/services/firebase.config.ts
+++ src/services/firebase.config.ts
@@ -1,16 +1,17 @@
-import firebase from 'firebase';
-import 'firebase/app';
+import firebase from 'firebase/app';
+import 'firebase/database';
 import 'firebase/auth';
 
-// const config  = {
-//     apiKey: process.env.REACT_APP_API_KEY,
-//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_PROJECT_ID,
-//     storageBucket: process.env.REACT_STORAGE_BUCKET,
-//     messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_APP_ID
-// }
+const config  = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+};
 
-// firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-export  const realtime = firebase;
+export const auth = firebase.auth();
+export  const realtime = firebase.database();
