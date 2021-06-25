6fe49c6a84dd66ab515bc9ff198d6d3788b6f9b6
xSallus <souza95salomao@gmail.com>
2021-06-25 16:15:33 -0400

    [UPDATE] Chat room now listen to url params

--- src/pages/ChatRoom/index.tsx
+++ src/pages/ChatRoom/index.tsx
@@ -1,11 +1,14 @@
+import { useParams } from "react-router-dom";
 import { chatRoomStyles } from "src/styles/chatroom.styles"
+import { IParams } from "src/types";
 
 export const ChatRoom = () => {
     const { chatRoomContainer } = chatRoomStyles();
+    const { id } = useParams() as IParams;
 
     return (
         <div className={chatRoomContainer} >
-            <h2>Chat Room</h2>
+            <h2>Chat Room: {id}</h2>
         </div>
     )
 }
